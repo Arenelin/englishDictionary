270e1db9c57e2cdbb428d7ef72ae7ed51c067e37
Nikita <belov.1098@mail.ru>
2023-08-23 13:06:23 +0700

    created css

--- js/main.js
+++ js/main.js
@@ -5,11 +5,21 @@ let result = document.querySelector('.result');
 
 for (let i = 0; i < inputs.length; i++){
    inputs[i].addEventListener('keydown', (e) => {
-      if (!isNaN(e.key)) {
+      if (!isNaN(e.key) || e.key === '+'
+         || e.key === '.'
+         || e.key === '-'
+         || e.key === '*'
+         || e.key === '/'
+         || e.key === '='
+         || e.key === '%'
+         || e.key === '$'
+         || e.key === '№'
+         || e.key === '!'
+         || e.key === '@'
+         || e.key === '/') {
          e.preventDefault();
       }
-      button.removeAttribute('disabled');
-   })
+   });
 }
 const btn = document.createElement('button');
 let countResponses = 0;
@@ -29,57 +39,79 @@ let button = document.querySelector('.btn-submit');
 button.addEventListener('click', find);
 
 function find() {
+   for (let i = 0; i < inputs.length; i++){
+      inputs[i].setAttribute('disabled', '');
+      inputs[i].style.cssText = `
+      box-shadow: 1px 1px 10px 5px rgb(236, 107, 107)
+      `;
+   }
    if (inputOne.value === 'Она' || inputOne.value === 'она') {
       countResponses++;
+      createShadow(inputOne);
    }
    if (inputTwo.value === 'Тремор' || inputTwo.value === 'тремор') {
       countResponses++;
+      createShadow(inputTwo);
    }
    if (inputThree.value === 'Осадки' || inputThree.value === 'осадки') {
       countResponses++;
+      createShadow(inputThree)
    }
    if (inputFour.value === 'Клад' || inputFour.value === 'клад') {
       countResponses++;
+      createShadow(inputFour)
    }
    if (inputFive.value === 'Утомительно' || inputFive.value === 'утомительно') {
       countResponses++;
+      createShadow(inputFive)
    }
    if (inputSix.value === 'Шрам' || inputSix.value === 'шрам') {
       countResponses++;
+      createShadow(inputSix)
    }
    if (inputSeven.value === 'Сцена' || inputSeven.value === 'сцена') {
       countResponses++;
+      createShadow(inputSeven)
    }
    if (inputEight.value === 'Записывать' || inputEight.value === 'записывать') {
       countResponses++;
+      createShadow(inputEight)
    }
    if (inputNine.value === 'История' || inputNine.value === 'история') {
       countResponses++;
+      createShadow(inputNine)
    }
    if (inputTen.value === 'Имманентный' || inputTen.value === 'имманентный') {
       countResponses++;
-   }
-   for (let i = 0; i < inputs.length; i++){
-      // inputs[i].value = '';
-      inputs[i].setAttribute('disabled', '');
+      createShadow(inputTen)
    }
    result.textContent = `Ваш итоговый результат теста: ${countResponses} баллов`;
    btn.className = 'btn-for-return';
    btn.textContent = 'Начать тест заново';
    result.append(btn);
+   button.setAttribute('disabled', '');
 }
 
 
-btn.addEventListener('click', clear)
+btn.addEventListener('click', clear);
 
 
 function clear (){
    result.textContent = '';
    countResponses = 0;
+   button.removeAttribute('disabled');
    for (let i = 0; i < inputs.length; i++){
       inputs[i].removeAttribute('disabled');
       inputs[i].value = '';
-      button.setAttribute('disabled', '');
+      inputs[i].style.cssText = `
+      box-shadow: none
+      `;
    }
 }
 
+
+function createShadow(input) {
+   input.style.cssText = `
+   box-shadow: 1px 1px 10px 5px rgb(151, 243, 151)
+   `;
+}
